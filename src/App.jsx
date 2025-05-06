cbfa9764d342769effe49cb7f125239666ee0262
Devanshubist <devanshubisht66@gmail.com>
2025-05-06 12:42:04 +0530

    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+export default function App() {
+  return (
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    
+  </Routes>
+  );
+} 
+
+
